b59f80287d9431edacf733301801bb1e575e88e8
adiocwball <adiowsy@gmail.com>
2020-05-06 17:28:37 -0300

    Inserido Campo local na tabela Evento. Inserido função Delete

--- core/views.py
+++ core/views.py
@@ -51,12 +51,21 @@ def evento(request):
 def submit_evento(request):
     if request.POST:
         titulo = request.POST.get('titulo')
+        local = request.POST.get('local')
         data_evento = request.POST.get('data_evento')
         descricao = request.POST.get('descricao')
         usuario = request.user
         Evento.objects.create(titulo=titulo,
+                              local = local,
                               data_evento=data_evento,
                               descricao=descricao,
                               usuario=usuario)
     return redirect('/')
 
+@login_required(login_url='/login/')
+def delete_evento(request, id_evento):
+    usuario = request.user
+    evento = Evento.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+        evento.delete()
+    return redirect('/')
