9bbc8b474a490773691f176095c9ee5672fdbec2
adiocwball <adiowsy@gmail.com>
2020-05-07 10:41:33 -0300

    Adicionado retorno de lista por Json, retorno de http004 e configurado timezone para pt-br

--- agenda/urls.py
+++ agenda/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 from django.views.generic import RedirectView
-
+from django.http.response import Http404, JsonResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
+    path('agenda/lista/<int:id_usuario>/', views.json_lista_evento),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
     path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
